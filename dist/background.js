739c1c969b562d774d1ea00b73c48823b5b112ea
jigsaw <m@jgs.me>
2022-09-01 19:25:55 +0900

    Upgrade V3

--- dist/background.js
+++ dist/background.js
@@ -1,20 +1,20 @@
-chrome.browserAction.onClicked.addListener(tab => {
-  chrome.tabs.sendMessage(tab.id, {type: 'rscrp:fetchPage'})
-})
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.sendMessage(tab.id, { type: "rscrp:fetchPage" });
+});
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   switch (message.type) {
-    case 'rscrp:pageFetched:default': {
+    case "rscrp:pageFetched:default": {
       chrome.tabs.create({
-        url: 'https://scrapbox.io/rac10/new'
-      })
-      break
+        url: "https://scrapbox.io/jigsaw/new",
+      });
+      break;
     }
-    case 'rscrp:pageFetched:withName': {
+    case "rscrp:pageFetched:withName": {
       chrome.tabs.create({
-        url: `https://scrapbox.io/${message.name}/new`
-      })
-      break
+        url: `https://scrapbox.io/${message.name}/new`,
+      });
+      break;
     }
   }
-})
+});
--- dist/content.js
+++ dist/content.js
@@ -1,29 +1,29 @@
-const el = document.createElement('script')
-el.src = chrome.extension.getURL('inject.js')
-document.head.appendChild(el)
+const el = document.createElement("script");
+el.src = chrome.runtime.getURL("inject.js");
+document.head.appendChild(el);
 
-const el1 = document.createElement('link')
-el1.href = chrome.extension.getURL('style.css')
-el1.rel = 'stylesheet'
-document.head.appendChild(el1)
+const el1 = document.createElement("link");
+el1.href = chrome.runtime.getURL("style.css");
+el1.rel = "stylesheet";
+document.head.appendChild(el1);
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === 'rscrp:fetchPage') {
-    const ev = new CustomEvent('rscrp:fetchPage')
-    window.dispatchEvent(ev)
+  if (message.type === "rscrp:fetchPage") {
+    const ev = new CustomEvent("rscrp:fetchPage");
+    window.dispatchEvent(ev);
   }
-})
+});
 
-window.addEventListener('message', ev => {
-  if (ev.data.type === 'rscrp:pageFetched:default') {
+window.addEventListener("message", (ev) => {
+  if (ev.data.type === "rscrp:pageFetched:default") {
     chrome.runtime.sendMessage(chrome.runtime.id, {
-      type: 'rscrp:pageFetched:default'
-    })
+      type: "rscrp:pageFetched:default",
+    });
   }
-  if (ev.data.type === 'rscrp:pageFetched:withName') {
+  if (ev.data.type === "rscrp:pageFetched:withName") {
     chrome.runtime.sendMessage(chrome.runtime.id, {
-      type: 'rscrp:pageFetched:withName',
-      name: ev.data.name
-    })
+      type: "rscrp:pageFetched:withName",
+      name: ev.data.name,
+    });
   }
-})
+});
